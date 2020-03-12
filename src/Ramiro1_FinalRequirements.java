9933e42b062ade34a5c6d7b3e0dc80066efe7edb
Default <Default@JamerPogi>
2020-03-12 08:48:17 +0800

    MyFirstProject

--- src/Ramiro1_FinalRequirements.java
+++ src/Ramiro1_FinalRequirements.java
@@ -5,8 +5,6 @@
 
 /**
 * hello sir Ahmad this is my code for the final requirements i told you before can you judge this if this enough
-* the you sent on me that you made that before was totally accept by my Prof but i try to make code ny myself for this
-*/
 
 /**
  * thanks for the help
